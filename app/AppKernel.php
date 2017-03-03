36074e4dec427198651ea463a7a6690a260209c2
pifaace <maximejoassy@hotmail.fr>
2017-03-03 17:45:46 +0100

    feature:navbar (#3)
    ajout de la navbar
        ajout des blocks assetic
        ajout de bootstrap

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,8 +16,8 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new Piface\AppBundle\PifaceAppBundle(),
-            new Symfony\Bundle\AsseticBundle\AsseticBundle(),
 
+            new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new FOS\UserBundle\FOSUserBundle(),
 
             new Piface\UserBundle\PifaceUserBundle(),
